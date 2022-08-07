666aef84072c3990a915873fa6144553bd205506
YuchenKwek <101268796+YuchenKwek@users.noreply.github.com>
2022-08-07 17:14:50 +0800

    Add files via upload

---
+++ Jupyter Assignment.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Name : Yu Chen**
+# 
+# Current occupation : Data Analyst Intern
+
+# *I am interested in data science because it allows me to discover some insights other people don't see and it makes me feel good to dive into the data-driven world.*
+
+# ### Let's do some simple math.
+
+# In[3]:
+
+
+1 + 1
+
+
+# ## My ~~To-Do~~ne List : 
+# ---
+# 1. Introduction
+# 2. Problem Background
+# 3. Methodology
+# 4. Analysis : Off-grid Village Mapping in West Malaysia
+#  * Pahang
+#  - Kelantan
+#  + Perak
+# 5. Conclusion  
+#    
+# ---
+# `About the Project`
+# * Date : 7th August 2022
+# * Version : 1st edition
+# * Author : Yu Chen
+# 
+# 
+# More Info : [Click Here]
+# 
+# [Click Here]: https://www.petronas.com/media/press-release/petronas-brings-rays-hope-sinar-harapan-life-orang-asli-and-community-based
+
+# In[ ]:
+
+
+
+
